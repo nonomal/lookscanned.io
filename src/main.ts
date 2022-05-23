c0b053c2621df0e191ab478a8bd94861936ad47e
Seedgou <i@zczc.cz>
2022-05-23 11:49:50 +0000

    add gtag

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { createMetaManager } from "vue-meta";
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router).use(createMetaManager()).use(vuetify).mount("#app");
+createApp(App)
+  .use(router)
+  .use(createMetaManager())
+  .use(VueGtag, {
+    config: { id: "G-0HFWPLP3L2" },
+  })
+  .use(vuetify)
+  .mount("#app");
